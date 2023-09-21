caf0d0f960de580e5dc6fa8c16a7a7891de5150d
Gokulmurugan1997 <Gokulanshvi@gmail.com>
2023-09-21 17:49:19 +0530

    update

--- json_resume/resume.js
+++ json_resume/resume.js
@@ -1,43 +1,45 @@
 let resume={
-    personaldetails:{
-        fname: "Gokul",
-        lname: "murugan.p",
-        email:"gokulmurugan@gmail.com",
-        phnum:"6383380034",
-        address:"erode, tamilnadu"
+    "personaldetails":{
+        "fname": "Gokul",
+        "lname": "murugan.p",
+        "email":"gokulmurugan@gmail.com",
+        "phnum":"6383380034",
+        "address":"erode, tamilnadu"
     },
-    education:{
-       schools: { 
-        instiution:"grace school",
-        percentage:"98%",
-        year:"2015"
+    "education":{
+       "schools": { 
+        "instiution":"grace school",
+        "percentage":"98%",
+        "year":"2015"
     },
-       college :{
-        university:"anna university",
-        branch:"mechanical",
-        percentage:"70%",
-        year:"2019"
+       "college" :{
+        "university":"anna university",
+        "branch":"mechanical",
+        "percentage":"70%",
+        "year":"2019"
        },
     },
-    skills:{
-        mechanical:{
-            core:"manufacturing,medical_devices,regulatory_affairs",
-            cad_tools:"creo, NX_unigraphics",
-            programming_languages:"python",
+    "skills":{
+        "mechanical":{
+            "core":"manufacturing,medical_devices,regulatory_affairs",
+            "cad_tools":"creo, NX_unigraphics",
+            "programming_languages":"python",
         
     },
     },
-    work:{
-        company:"Hcl tech",
-        role:"memeber technical staff",
-        year_of_exp:"3 years",
-        startdate:"dec_2019",
-        enddate:"till today"
+    "work":{
+        "company":"Hcl tech",
+        "role":"memeber technical staff",
+        "year_of_exp":"3 years",
+        "startdate":"dec_2019",
+        "enddate":"till today"
   },
-    interests:{
-        studies:"coding",
-        games:"solivingpuzzles,cricket"
+    "interests":{
+        "studies":"coding",
+        "games":"solivingpuzzles,cricket"
     }
 
 }
-console.log(resume);
+for (let key in resume){
+    console.log(key);
+}
